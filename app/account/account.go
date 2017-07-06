fbb135fdf038df793e3780ae82e2542c8a0e51d9
Agustin Sarasua <asarasua@despegar.com>
2017-07-06 18:03:03 -0300

    fixes

--- app/account/account.go
+++ app/account/account.go
@@ -9,3 +9,20 @@ type Account struct {
 	Name        string    `json:"name" bson:"name"`
 	CreatedDate time.Time `json:"createdDate,omitempty" bson:"createdDate"`
 }
+
+type CreditCard struct {
+	ID         string `json:"id" bson:"_id"`
+	Bin        string `json:"bin" bson:"bin"`
+	LastDigits string `json:"lastDigits" bson:"lastDigits"`
+	Type       string `json:"type" bson:"type"`
+	Prepaid    bool   `json:"prepaid" bson:"prepaid"`
+	Brand      string `json:"brand" bson:"brand"`
+	Bank       struct {
+		Name  string `json:"name"`
+		URL   string `json:"url"`
+		Phone string `json:"phone"`
+		City  string `json:"city"`
+	} `json:"bank"`
+	CountryCode string    `json:"countryCode" bson:"countryCode"`
+	CreatedDate time.Time `json:"createdDate" bson:"createdDate"`
+}
